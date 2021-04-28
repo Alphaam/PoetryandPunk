ba3f46f88c7f8c9631f61e2cfaf666726d2d1bfd
Alphaam <58554818+Alphaam@users.noreply.github.com>
2021-04-29 03:50:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+
+function nextMsg1() {
+    if (messages.length == 0) {
+        // once there is no more message, do whatever you want
+        // alert("redirecting");
+    } else {
+        // change content of message, fade in, wait, fade out and continue with next message
+        $('#message').html(messages.pop()).fadeIn(500).delay(1000).fadeOut(500, nextMsg1);
+        
+    }
+};
